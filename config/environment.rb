80b80c0d79eaa919bd07372bc606229e2330d74e
Ian Miller <irmiller22@gmail.com>
2014-11-05 10:42:27 -0500

    updated environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,5 +3,5 @@ require 'pry'
 
 Bundler.require(:default)
 
-require './lib/even_fibonacci'
-require './lib/oo_even_fibonacci'
+require './lib/smallest_multiple'
+require './lib/oo_smallest_multiple'
