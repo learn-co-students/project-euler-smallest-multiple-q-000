78affdebe243ce5a276df4cb1244adfdf8dac0cb
uncledoofy156 <uncledoofy156@aim.com>
2015-11-18 19:47:14 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,25 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  attr_reader :lcm
+  def initialize(num)
+    @num = num
+    @lcm = smallest_multiple
+  end
+  def smallest_multiple
+    result = 1
+    (1..@num).each do |n|
+      result = result * n / gcd(result, n)
+    end
+    result
+  end
+
+  #greatest common denominator
+  #using Euclid Algorithm
+  def gcd(a,b)
+    while a % b != 0
+      a %= b
+      a,b = b,a
+    end
+    b
+  end
+end
