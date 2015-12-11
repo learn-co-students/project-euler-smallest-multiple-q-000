b8df24cda070c6070acddca9c4b8bb60911af435
CodrinMihaila <codrin@my.com>
2015-12-11 09:30:54 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,28 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize (input)
+    @input = input
+  end
+  def input=(input)
+    @input = input
+  end
+  def input
+    @input
+  end
+  def lcm
+  (1..1000000000).each do |x|
+     count = 0
+    (1..@input).each do |y|
+      if x % y != 0
+        count += 1
+      end
+    end
+    if count == 0
+      return x
+      break
+    end
+  end
+
+  
+end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,16 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+  (1..1000000000).each do |x|
+     count = 0
+    (1..input).each do |y|
+      if x % y != 0
+        count += 1
+      end
+    end
+    if count == 0
+      return x
+      break
+    end
+  end
+
+  
+end
