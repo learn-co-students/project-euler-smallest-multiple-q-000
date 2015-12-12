5e2098fef6c2d0676532d8e1a46aa859f14a21b0
EricMoy <uncledoofy156@aim.com>
2015-12-12 03:15:09 -0500

    Update smallest_multiple.rb

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,18 +1,4 @@
 # Implement your procedural solution here!
 def smallest_multiple(num)
-  result = 1
-  (1..num).each do |n|
-    result = result * n / gcd(result, n)
-  end
-  result
-end
-
-#greatest common denominator
-#using Euclid Algorithm
-def gcd(a,b)
-  while a % b != 0
-    a %= b
-    a,b = b,a
-  end
-  b
+  (1..num).inject(1) { |res, el| (el*res)/res.gcd(el)}
 end
