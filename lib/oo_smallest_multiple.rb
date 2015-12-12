60c494c30e71e985f9f8d9cafa0f2e65b29d3fac
EricMoy <uncledoofy156@aim.com>
2015-12-12 03:22:56 -0500

    Update oo_smallest_multiple.rb

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -6,20 +6,6 @@ class SmallestMultiple
     @lcm = smallest_multiple
   end
   def smallest_multiple
-    result = 1
-    (1..@num).each do |n|
-      result = result * n / gcd(result, n)
-    end
-    result
-  end
-
-  #greatest common denominator
-  #using Euclid Algorithm
-  def gcd(a,b)
-    while a % b != 0
-      a %= b
-      a,b = b,a
-    end
-    b
+    (1..@num).inject(1) { |res, el| (el*res)/res.gcd(el)}
   end
 end
