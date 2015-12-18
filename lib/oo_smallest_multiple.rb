8e80e949acede9a52f2c2d4c41a6156b97cb32de
azheng249 <andy.zheng249@gmail.com>
2015-12-18 20:38:29 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,12 @@
 # Implement your object-oriented solution here!
+
+require_relative 'smallest_multiple'
+
+class SmallestMultiple
+  attr_accessor :lcm
+  
+  def initialize(input=1)
+    @lcm = smallest_multiple(input)
+  end
+  
+end
