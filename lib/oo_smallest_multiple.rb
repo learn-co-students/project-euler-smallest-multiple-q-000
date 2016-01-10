11099d9c301bb5bea1fd86c3450c16921d893ee9
parora86 <parora.cpa@outlook.com>
2016-01-10 05:27:43 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,31 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize(multiple_int)
+    @multiple = multiple_int
+  end
+
+  def multiple_getter()
+    @multiple
+  end
+
+  def multiple_setter(multiple_int)
+    @multiple = multiple_int
+  end
+
+  def lcm()
+    count = 0
+    value = multiple_getter()
+    while (count < multiple_getter()) do
+      count += 1
+      lcm=false
+      while (lcm==false) do
+        if (value%count==0)
+          lcm=true
+        else
+          value += multiple_getter()
+          count = 1
+        end
+      end
+    end
+    return value
+  end
+end
