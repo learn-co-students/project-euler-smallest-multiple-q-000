39b2e6e185ec72f56f93c560ad1569c7970f9bad
bradleyoashburn <bradleyoashburn@gmail.com>
2016-02-29 01:26:06 -1000

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,26 @@
 # Implement your procedural solution here!
+def smallest_multiple(input)
+  num = input
+
+  while true
+    if isDivisible(num, input)
+      return num
+    else
+      num += input
+    end
+  end
+
+end
+
+def isDivisible(num, input)
+  divisible = true
+
+  (2..input).each {|divisor|
+    if !(num % divisor == 0)
+      divisible = false
+    end
+  }
+
+  return divisible
+end
+
