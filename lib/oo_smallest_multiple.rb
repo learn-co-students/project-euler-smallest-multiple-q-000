0d91909843589d7e8a8734e934d12504b3c27c65
bmolina-nyc <bruce.d.molina@gmail.com>
2016-02-29 21:43:20 -0500

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+
+class SmallestMultiple
+
+
+  def initialize(number)
+    @number = number 
+    @answer = number 
+  end
+
+  def lcm
+    i = 1 
+    until i == @number 
+      if @answer % i == 0 
+        i += 1 
+      else
+        i = 1 
+        @answer += @number 
+      end
+    end
+    @answer
+  end
+
+end
+
+
+
+
+
+
