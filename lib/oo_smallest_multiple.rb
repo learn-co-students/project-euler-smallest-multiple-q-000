afeb257f9f37dd66e85eb1bbc7f5d13ac5c1d51f
einarstensson <einar.stensson@gmail.com>
2016-03-21 18:37:29 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,19 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  attr_reader :lcm
+
+  def initialize(max)
+    @max, @candidate = max, max
+    @divisors = (2..max)
+    @lcm = smallest_multiple
+  end
+
+  def smallest_multiple
+    while true
+      if @divisors.all?{ |divisor| @candidate % divisor == 0 }
+        return @candidate
+      else
+        @candidate += @max
+      end
+    end
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,14 @@
-# Implement your procedural solution here!
+def smallest_multiple(max)
+  candidate = max
+  divisors = (2..max)
+
+  while true
+    if divisors.all?{ |divisor| candidate % divisor == 0 }
+      return candidate
+    else
+      candidate += max
+    end
+  end
+end
+
+
