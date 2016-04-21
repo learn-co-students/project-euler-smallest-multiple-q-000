99778c75c181225dde25cf18e0f9d3e1e1889bb0
ollieshmollie <olishmollie@gmail.com>
2016-04-20 23:55:56 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+  def initialize(limit)
+    @limit = limit
+    @divisors = (1..limit).to_a
+    @dividends = [limit]
+  end
+
+  def lcm
+    @dividends.each do |dividend|
+      if !@divisors.all?{|divisor| dividend %divisor == 0}
+        @dividends.push(dividend + @limit)
+      else
+        return dividend
+      end
+    end
+  end
+end
+
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,11 @@
 # Implement your procedural solution here!
+def smallest_multiple(limit)
+  divisors = (1..limit).to_a
+  dividends = [limit]
+  dividends.each do |dividend|
+    if !divisors.all?{|divisor| dividend %divisor == 0}
+      dividends.push(dividend + limit) 
+    else return dividend
+    end
+  end
+end
