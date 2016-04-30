fc284559e13d8d0a1f1d1167e142894dec7a55c2
thedailyephemera <thedailyephemera@gmail.com>
2016-04-30 14:16:09 -0400

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,8 @@
 # Implement your procedural solution here!
+def smallest_multiple(limit)
+	increment = limit
+	while !(1..limit).all?{|div| increment % div == 0}
+		increment += limit
+	end
+	return increment
+end
