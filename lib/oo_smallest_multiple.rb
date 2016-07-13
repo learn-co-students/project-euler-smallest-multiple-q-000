6b4e1145cf75b09875da144d208d299ab0932813
Pamela Alvarez <p.stacey.a@gmail.com>
2016-07-13 02:36:56 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,17 @@
 # Implement your object-oriented solution here!
+
+
+class SmallestMultiple
+
+  def initialize(input)
+      @input = input
+    end
+
+
+  def lcm
+    (1..@input).to_a.reduce(:lcm)
+  end
+
+
+
+end
