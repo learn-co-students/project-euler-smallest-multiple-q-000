3c21964a5cda9e9869ff33d7a990a13e7435fe64
Eoghan Leddy <eleddy521@gmail.com>
2016-08-03 00:08:44 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,30 @@
 # Implement your object-oriented solution here!
+DENOMINATOR_ARRAY = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+class SmallestMultiple
+
+  def initialize(input)
+
+    @denom_arr = DENOMINATOR_ARRAY.select do |x|
+      x <= input
+    end
+    @input = input
+  end
+
+  def lcm
+
+    multiple = @input
+    until(check_denoms(multiple) == true)
+      multiple += @input
+    end
+    multiple
+
+  end
+
+  def check_denoms(num_to_chk)
+
+    @denom_arr.all? do |x|
+      num_to_chk % x == 0
+    end
+  end
+
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,22 @@
 # Implement your procedural solution here!
+DENOMINATOR_ARRAYS = [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+def smallest_multiple(input)
+
+  multiple = input
+  until(check_denoms(multiple, input) == true)
+    multiple += input
+  end
+  multiple
+
+
+end
+def check_denoms(num_to_chk, input)
+
+  denom_arr = DENOMINATOR_ARRAYS.select do |x|
+    x <= input
+  end
+  denom_arr.all? do |x|
+    num_to_chk % x == 0
+  end
+
+end
