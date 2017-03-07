042086613840c0a4478db727c0ce9b7d30d8c1da
Levio Marotti III <lm3@protonmail.com>
2017-03-06 23:30:07 -0600

    Done.

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,17 @@
 # Implement your procedural solution here!
+def smallest_multiple(input)
+  placeholder = 1
+  (2..input).each do |i|
+    placeholder *= i / gcd(placeholder, i)
+  end
+  placeholder
+end
+
+def gcd(a, b)
+  while b > 0
+    a %= b
+    return b if a == 0
+    b %= a
+  end
+  a
+end
