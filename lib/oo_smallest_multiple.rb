998e2d7aaed61e21296ebad7c1ad9d21feed1667
Olia Tkachenko <liaolya@ukr.net>
2017-03-14 19:10:37 +0000

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,31 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+def initialize(p)
+  @p = p
+end
+
+def gcd(a, b)
+while b != 0 do
+a = a% b
+a = a^b
+b = b^a
+a = a^b
+end
+return a
+end
+
+def lcmd(a, b)
+return a / gcd(a, b) * b
+end
+
+def lcm
+res = 1
+for i in 2 ..@p
+res = lcmd(res, i)
+end
+return res
+end
+end
+
+SmallestMultiple.new(5).lcm
