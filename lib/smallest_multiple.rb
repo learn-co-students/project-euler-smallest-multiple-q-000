81c83f414f88b169ea66751e57408e075af19369
SiamKing <thesiameseelectric@gmail.com>
2017-03-21 15:30:39 -0700

    L to the C to the M

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,13 @@
-# Implement your procedural solution here!
+def smallest_multiple(input)
+  arr = (1..input).to_a
+  i = input
+
+  loop do
+    if arr.all? {|num| i % num == 0}
+      return i
+      break
+    end
+    i += input
+  end
+  i
+end
