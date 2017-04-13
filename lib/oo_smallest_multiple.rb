2a8a2c0fc584075535901abadeda9b148ac809ab
sami4thewin <samiomarmohammed@gmail.com>
2017-04-12 22:58:53 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,18 @@
 # Implement your object-oriented solution here!
+class SmallestMultiple
+
+  attr_accessor :num
+
+  def initialize(num)
+    @num = num
+  end
+
+  def lcm
+    x = num
+    until (1..num).to_a.select{|y| x % y == 0 }.length == num
+      x += num
+    end
+    x 
+  end
+
+end
