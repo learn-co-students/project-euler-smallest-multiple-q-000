0e34d927fad2fefeafd2debd6badbbd044b67225
mirandalily <mirandaraymond@me.com>
2017-06-12 17:35:23 -0700

    lcm passing but in an infinite loop i think

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -8,6 +8,15 @@ class SmallestMultiple
   def lcm
     num = 1
     while (true)
+      for i in 1..@int
+        if (num % i != 0)
+          num += 1
+          break
+        elsif (i == @int)
+          return num
+        end
+      end
+    end
   end
 
 end
