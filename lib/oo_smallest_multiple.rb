f77c6cd6a1b0d12c85a792f6c99c44aa52927c23
mirandalily <mirandaraymond@me.com>
2017-06-12 17:33:07 -0700

    setting up lcm

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,12 +1,13 @@
 # Implement your object-oriented solution here!
 class SmallestMultiple
 
-  def initialize
-
+  def initialize(int)
+    @int = int
   end
 
   def lcm
-
+    num = 1
+    while (true)
   end
 
 end
