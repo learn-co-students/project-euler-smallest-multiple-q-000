14342a1cfeab617f108a4d868a6ad2b23485098a
mirandalily <mirandaraymond@me.com>
2017-06-12 17:45:11 -0700

    done

--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -3,5 +3,12 @@ def smallest_multiple(int)
   num = 1
   while (true)
     for i in 1..int
-
+      if (num % i != 0)
+        num += 1
+        break
+      elsif (i == int)
+        return num
+      end
+    end
+  end
 end
