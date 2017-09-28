3f1304e500178ff0fba5c9cee4b3f54dc629a20d
Ann Lee <aplee29@users.noreply.github.com>
2017-09-27 23:16:50 -0400

    Done.

--- lib/oo_smallest_multiple.rb
+++ lib/oo_smallest_multiple.rb
@@ -1,1 +1,24 @@
-# Implement your object-oriented solution here!
+class SmallestMultiple
+  attr_accessor :lcm
+
+  def initialize(inp)
+    @placeholder = 1
+    @lcm = find_multiple(inp)
+  end
+
+  def find_multiple(inp)
+    (2..inp).each do |i|
+      @placeholder *= i / gcd(@placeholder, i)
+    end
+    @placeholder
+  end
+
+  def gcd(x, y)
+    while y > 0
+      x %= y
+      return y if x == 0
+      y %= x
+    end
+    x
+  end
+end
--- lib/smallest_multiple.rb
+++ lib/smallest_multiple.rb
@@ -1,1 +1,16 @@
-# Implement your procedural solution here!
+def smallest_multiple(inp)
+  placeholder = 1
+  (2..inp).each do |i|
+    placeholder *= i / gcd(placeholder, i)
+  end
+  placeholder
+end
+
+def gcd(x, y)
+  while y > 0
+    x %= y
+    return y if x == 0
+    y %= x
+  end
+  x
+end
